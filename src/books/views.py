d6cadecf41b6ca33e6dba5fe28ba2a67521a32af
AdamMisiak <adammisiak3@gmail.com>
2020-05-28 18:55:32 +0200

    book details view added

--- src/books/views.py
+++ src/books/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Book
 
 # Create your views here.
+def book_detail_view(request):
+	book = Book.objects.get(id=1)
+	contex = {
+		'title': book.title,
+		'author': book.author,
+		'sites': book.sites,
+
+
+	}
+	return render(request, 'book.html', contex)
