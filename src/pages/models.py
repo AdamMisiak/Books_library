37f68023c8cf44d3d86dd6e72e48463bc29d9f84
AdamMisiak <adammisiak3@gmail.com>
2020-08-08 16:01:08 +0200

    UserPhoto model added

--- src/pages/models.py
+++ src/pages/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserPhoto(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='static/images',
+                              default='https://recap-project.eu/wp-content/uploads/2017/02/default-user.jpg',)
