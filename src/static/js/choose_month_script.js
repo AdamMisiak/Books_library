526f8fca51f3e87a3af6c4c86b3cb4646baab25c
AdamMisiak <adammisiak3@gmail.com>
2020-08-27 19:56:02 +0200

    Js month function upgraded

--- src/static/js/choose_month_script.js
+++ src/static/js/choose_month_script.js
@@ -1,7 +1,16 @@
+const capitalize = (s) => {
+  if (typeof s !== 'string') return ''
+  return s.charAt(0).toUpperCase() + s.slice(1)
+}
+
 function chooseMonth(clicked_id) {
-  month = clicked_id
-
-  console.log(month);
-  console.log(document.getElementById('test'));
-
+    month = capitalize(clicked_id)
+    console.log(month)
+    elements = document.querySelectorAll('[id=month]');
+    for (var i = 0; i < elements.length; i++) {
+        if (String(elements[i].innerHTML) == String(month)) {
+        console.log('test')
+        }
+        console.log(elements[i].innerHTML);
+    }
 }
