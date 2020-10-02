6b91d2ce38f2b3df7d3ea64efcc0d964df6dd6d3
AdamMisiak <adammisiak3@gmail.com>
2020-10-02 09:20:47 +0200

    Admin tools for books and books positions upgraded

--- src/books/admin.py
+++ src/books/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
 from .models import Book, BookPosition
 
-admin.site.register(Book)
-admin.site.register(BookPosition)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'author', 'genre_1', 'description')
+    list_display_links = ('id', 'title')
+    list_filter = ('genre_1', )
+    search_fields = ('title', 'author')
+    list_per_page = 20
+
+class BookPositionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'book', 'value', 'month', 'year', 'status', 'review')
+    list_display_links = ('id', 'user')
+    list_filter = ('month', 'year', 'status')
+    list_per_page = 20
+
+
+
+admin.site.register(Book, BookAdmin)
+admin.site.register(BookPosition, BookPositionAdmin)
